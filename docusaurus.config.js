fcfc0b12dafcb384e6b9f1ba8d833a19e872c815
Dinh Bao Dang <250694+chillinpanda@users.noreply.github.com>
2023-10-06 10:27:50 +0200

    setup GitHub Pages and Actions

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://diva-e.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/usercentrics-adobe-launch-extension-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'diva-e',
-  projectName: 'usercentrics-extension-documentation', // Usually your repo name.
+  projectName: 'usercentrics-adobe-launch-extension-documentation', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -42,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/diva-e/usercentrics-adobe-launch-extension-documentation/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/diva-e/usercentrics-adobe-launch-extension-documentation/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
